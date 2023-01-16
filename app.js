26d39bbd1c77d3dd5e790680041fd538fb9a7923
Yohannes Bekele <yohannesbekele81@gmail.com>
2023-01-16 17:06:23 +0300

    updated zoom

--- app.js
+++ app.js
@@ -16,15 +16,44 @@ const notFound = require('./middleware/notfound')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
 const app = express();
+// live server
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const { v4: uuidV4 } = require('uuid');
+app.set('view engine', 'ejs');
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+});
+
+
 app.use(express.static('./public'));
 app.use(morgan('tiny'));
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.json());
 app.use(fileUpload({ createParentPath: true }))
-app.get('/', (req, res) => {
-    res.send('e-learning')
-});
 
+app.get('/zoom', (req, res) => {
+    res.redirect(`/zoom${uuidV4()}`)
+})
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+})
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        console.log(`user ${userId} joined ${roomId}`);
+        socket.join(roomId);
+        socket.on('ready', () => {
+            socket.broadcast.to(roomId).emit('user-connected', userId);
+        })
+        socket.on('disconnect', () => {
+            socket.to(roomId).emit('user-disconnected', userId)
+        })
+    })
+
+})
+app.use('/peerjs', peerServer);
 app.use('/users', userRouter);
 app.use('/courses', courseRouter);
 app.use('/instractors', instractorRouter);
@@ -39,7 +68,7 @@ const port = process.env.port || 3000;
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI);
-        app.listen(port, console.log(`server is listening port ${port}... `));
+        server.listen(port, console.log(`server is listening port ${port}... `));
     } catch (error) {
         console.log(error);
     }
